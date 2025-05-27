d8e459db5fc51650279d3e645054450880ce3f7b
김주엽 <kjyy08@naver.com>
2025-05-27 14:34:27 +0900

    remove: CustomHttpClient 및 관련 구현 클래스 제거

--- src/main/java/dev/luigi/slack/mcp/server/dto/common/HttpClientRequest.java
+++
@@ -1,8 +0,0 @@
-package dev.luigi.slack.mcp.server.dto.common;
-
-import lombok.Builder;
-import org.springframework.http.HttpMethod;
-
-@Builder
-public record HttpClientRequest(String url, HttpMethod method, Object requestBody, String... headers) {
-}
--- src/main/java/dev/luigi/slack/mcp/server/util/CustomHttpClient.java
+++
@@ -1,9 +0,0 @@
-package dev.luigi.slack.mcp.server.util;
-
-import dev.luigi.slack.mcp.server.dto.common.HttpClientRequest;
-
-public interface CustomHttpClient {
-    String sendRequest(HttpClientRequest httpClientRequest) throws RuntimeException;
-
-    String sendMultipartRequest(HttpClientRequest httpClientRequest) throws RuntimeException;
-}
